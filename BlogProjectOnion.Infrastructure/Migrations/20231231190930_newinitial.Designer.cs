53ba9dd5fd6758316940bf6d018d572fede429aa
Eren Çolak <eren.colk01@gmail.com>
2023-12-31 22:13:10 +0300

    ENDD

--- BlogProjectOnion.Infrastructure/Migrations/20231231190930_newinitial.Designer.cs
+++ BlogProjectOnion.Infrastructure/Migrations/20231231190930_newinitial.Designer.cs
@@ -4,6 +4,7 @@ using BlogProjectOnion.Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BlogProjectOnion.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231231190930_newinitial")]
+    partial class newinitial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
