81405972c2c97af14eaabeb5599d4f67be2cb28c
Eren Çolak <eren.colk01@gmail.com>
2024-01-01 10:12:34 +0300

    some files were deleted

---
+++ BlogProjectOnion.Presentation/wwwroot/Ready-Bootstrap-Dashboard-master/assets/js/comment.js
@@ -0,0 +1,19 @@
+﻿function ViewComments(id) {
+
+    $.ajax({
+        url: '/admin/post/PostDetailComments/' + id,
+        type: "GET",
+
+        success: function (response) {
+            $("#CommentsView").html(response)
+        },
+        error: function () {
+
+            console.log("Hata");
+        }
+
+
+    })
+
+
+}
